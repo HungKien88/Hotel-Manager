5c21384ad3aac741caf718996324a55141d82329
TienNguyen <nguyenngocthuytien1994@gmail.com>
2019-09-13 22:32:12 +0700

    aaa

--- PhanMemQuanLyKhachSan_17DTHC6/PhanMemQuanLyKhachSan/ManHinhChinh.cs
+++ PhanMemQuanLyKhachSan_17DTHC6/PhanMemQuanLyKhachSan/ManHinhChinh.cs
@@ -16,5 +16,13 @@ namespace PhanMemQuanLyKhachSan
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            ////  DASDSADAAS
+           
+        }
     }
 }
