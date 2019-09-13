2b6d64af0e87b493f6a2ba15684249725ac48d43
phuonghuyit2205 <phuongitchat2205@gmail.com>
2019-09-13 22:27:26 +0700

    Phuong sua lan 4

--- PhanMemQuanLyKhachSan_17DTHC6/PhanMemQuanLyKhachSan/QuanLyNhanVien.Designer.cs
+++ PhanMemQuanLyKhachSan_17DTHC6/PhanMemQuanLyKhachSan/QuanLyNhanVien.Designer.cs
@@ -33,19 +33,21 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(40, 53);
+            this.button1.Location = new System.Drawing.Point(30, 43);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(56, 19);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // QuanLyNhanVien
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.ClientSize = new System.Drawing.Size(1205, 605);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "QuanLyNhanVien";
             this.Text = "QuanLyNhanVien";
             this.ResumeLayout(false);
