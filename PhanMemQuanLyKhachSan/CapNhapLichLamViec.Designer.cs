9b8c0c816e8ad44d4616ebdc7d13f7cc551a3a3e
phuonghuyit2205 <phuongitchat2205@gmail.com>
2019-10-02 16:26:08 +0700

    Phuong lam form Cap nhat lich làm viêc

---
+++ PhanMemQuanLyKhachSan/CapNhapLichLamViec.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace PhanMemQuanLyKhachSan
+{
+    partial class frmCapNhatLichLamViec
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblLichLamViec = new System.Windows.Forms.Label();
+            this.btnTroVeCuaCapNhatLichLamViec = new System.Windows.Forms.Button();
+            this.btnThemLichLamViec = new System.Windows.Forms.Button();
+            this.btnSuaLichLamViec = new System.Windows.Forms.Button();
+            this.btnXoaLichLamViec = new System.Windows.Forms.Button();
+            this.txtGhiChu = new System.Windows.Forms.TextBox();
+            this.lblGhiChu = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.lblCa = new System.Windows.Forms.Label();
+            this.lblCaSang = new System.Windows.Forms.Label();
+            this.lblCaTrua = new System.Windows.Forms.Label();
+            this.lblCaToi = new System.Windows.Forms.Label();
+            this.lblThu6 = new System.Windows.Forms.Label();
+            this.lblThu2 = new System.Windows.Forms.Label();
+            this.lblThu7 = new System.Windows.Forms.Label();
+            this.lblThu3 = new System.Windows.Forms.Label();
+            this.lblThu5 = new System.Windows.Forms.Label();
+            this.lblThu4 = new System.Windows.Forms.Label();
+            this.lblChuNhat = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblLichLamViec
+            // 
+            this.lblLichLamViec.AutoSize = true;
+            this.lblLichLamViec.Font = new System.Drawing.Font("Palatino Linotype", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLichLamViec.Location = new System.Drawing.Point(393, 23);
+            this.lblLichLamViec.Name = "lblLichLamViec";
+            this.lblLichLamViec.Size = new System.Drawing.Size(396, 44);
+            this.lblLichLamViec.TabIndex = 0;
+            this.lblLichLamViec.Text = "Lịch Làm Việc Trong Tuần";
+            // 
+            // btnTroVeCuaCapNhatLichLamViec
+            // 
+            this.btnTroVeCuaCapNhatLichLamViec.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTroVeCuaCapNhatLichLamViec.Image = global::PhanMemQuanLyKhachSan.Properties.Resources.iconBack;
+            this.btnTroVeCuaCapNhatLichLamViec.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnTroVeCuaCapNhatLichLamViec.Location = new System.Drawing.Point(205, 106);
+            this.btnTroVeCuaCapNhatLichLamViec.Name = "btnTroVeCuaCapNhatLichLamViec";
+            this.btnTroVeCuaCapNhatLichLamViec.Size = new System.Drawing.Size(101, 45);
+            this.btnTroVeCuaCapNhatLichLamViec.TabIndex = 10;
+            this.btnTroVeCuaCapNhatLichLamViec.Text = "Trở về";
+            this.btnTroVeCuaCapNhatLichLamViec.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnTroVeCuaCapNhatLichLamViec.UseVisualStyleBackColor = true;
+            // 
+            // btnThemLichLamViec
+            // 
+            this.btnThemLichLamViec.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThemLichLamViec.Image = global::PhanMemQuanLyKhachSan.Properties.Resources.iconThem;
+            this.btnThemLichLamViec.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnThemLichLamViec.Location = new System.Drawing.Point(392, 106);
+            this.btnThemLichLamViec.Name = "btnThemLichLamViec";
+            this.btnThemLichLamViec.Size = new System.Drawing.Size(109, 45);
+            this.btnThemLichLamViec.TabIndex = 11;
+            this.btnThemLichLamViec.Text = "Thêm ";
+            this.btnThemLichLamViec.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnThemLichLamViec.UseVisualStyleBackColor = true;
+            // 
+            // btnSuaLichLamViec
+            // 
+            this.btnSuaLichLamViec.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSuaLichLamViec.Image = global::PhanMemQuanLyKhachSan.Properties.Resources.iconSuaaaaa;
+            this.btnSuaLichLamViec.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSuaLichLamViec.Location = new System.Drawing.Point(852, 106);
+            this.btnSuaLichLamViec.Name = "btnSuaLichLamViec";
+            this.btnSuaLichLamViec.Size = new System.Drawing.Size(109, 45);
+            this.btnSuaLichLamViec.TabIndex = 13;
+            this.btnSuaLichLamViec.Text = "Sửa ";
+            this.btnSuaLichLamViec.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnSuaLichLamViec.UseVisualStyleBackColor = true;
+            // 
+            // btnXoaLichLamViec
+            // 
+            this.btnXoaLichLamViec.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXoaLichLamViec.Image = global::PhanMemQuanLyKhachSan.Properties.Resources.iconDelete;
+            this.btnXoaLichLamViec.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnXoaLichLamViec.Location = new System.Drawing.Point(643, 106);
+            this.btnXoaLichLamViec.Name = "btnXoaLichLamViec";
+            this.btnXoaLichLamViec.Size = new System.Drawing.Size(109, 45);
+            this.btnXoaLichLamViec.TabIndex = 12;
+            this.btnXoaLichLamViec.Text = "Xóa ";
+            this.btnXoaLichLamViec.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnXoaLichLamViec.UseVisualStyleBackColor = true;
+            // 
+            // txtGhiChu
+            // 
+            this.txtGhiChu.Location = new System.Drawing.Point(1487, 277);
+            this.txtGhiChu.Multiline = true;
+            this.txtGhiChu.Name = "txtGhiChu";
+            this.txtGhiChu.Size = new System.Drawing.Size(215, 185);
+            this.txtGhiChu.TabIndex = 14;
+            // 
+            // lblGhiChu
+            // 
+            this.lblGhiChu.AutoSize = true;
+            this.lblGhiChu.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGhiChu.Location = new System.Drawing.Point(1556, 252);
+            this.lblGhiChu.Name = "lblGhiChu";
+            this.lblGhiChu.Size = new System.Drawing.Size(68, 22);
+            this.lblGhiChu.TabIndex = 15;
+            this.lblGhiChu.Text = "Ghi Chú";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel1.ColumnCount = 8;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 41.81818F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 58.18182F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 173F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 179F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 190F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 195F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 180F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 184F));
+            this.tableLayoutPanel1.Controls.Add(this.lblChuNhat, 7, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lblCa, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lblThu7, 6, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lblThu5, 4, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lblThu6, 5, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lblThu4, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lblCaSang, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lblCaTrua, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.lblThu3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lblCaToi, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.lblThu2, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(54, 206);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 32.69231F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 67.30769F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 102F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 97F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1385, 341);
+            this.tableLayoutPanel1.TabIndex = 16;
+            // 
+            // lblCa
+            // 
+            this.lblCa.AutoSize = true;
+            this.lblCa.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCa.Location = new System.Drawing.Point(4, 1);
+            this.lblCa.Name = "lblCa";
+            this.lblCa.Size = new System.Drawing.Size(29, 22);
+            this.lblCa.TabIndex = 17;
+            this.lblCa.Text = "Ca";
+            // 
+            // lblCaSang
+            // 
+            this.lblCaSang.AutoSize = true;
+            this.lblCaSang.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCaSang.Location = new System.Drawing.Point(4, 46);
+            this.lblCaSang.Name = "lblCaSang";
+            this.lblCaSang.Size = new System.Drawing.Size(73, 44);
+            this.lblCaSang.TabIndex = 18;
+            this.lblCaSang.Text = "Sáng\r\n7h -> 15h\r\n";
+            // 
+            // lblCaTrua
+            // 
+            this.lblCaTrua.AutoSize = true;
+            this.lblCaTrua.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCaTrua.Location = new System.Drawing.Point(4, 139);
+            this.lblCaTrua.Name = "lblCaTrua";
+            this.lblCaTrua.Size = new System.Drawing.Size(81, 44);
+            this.lblCaTrua.TabIndex = 19;
+            this.lblCaTrua.Text = "Trưa\r\n15h -> 22h";
+            // 
+            // lblCaToi
+            // 
+            this.lblCaToi.AutoSize = true;
+            this.lblCaToi.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCaToi.Location = new System.Drawing.Point(4, 242);
+            this.lblCaToi.Name = "lblCaToi";
+            this.lblCaToi.Size = new System.Drawing.Size(69, 44);
+            this.lblCaToi.TabIndex = 20;
+            this.lblCaToi.Text = "Tối \r\n22h ->7h\r\n";
+            // 
+            // lblThu6
+            // 
+            this.lblThu6.AutoSize = true;
+            this.lblThu6.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblThu6.Location = new System.Drawing.Point(825, 1);
+            this.lblThu6.Name = "lblThu6";
+            this.lblThu6.Size = new System.Drawing.Size(50, 22);
+            this.lblThu6.TabIndex = 21;
+            this.lblThu6.Text = "Thứ 6";
+            // 
+            // lblThu2
+            // 
+            this.lblThu2.AutoSize = true;
+            this.lblThu2.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblThu2.Location = new System.Drawing.Point(119, 1);
+            this.lblThu2.Name = "lblThu2";
+            this.lblThu2.Size = new System.Drawing.Size(50, 22);
+            this.lblThu2.TabIndex = 22;
+            this.lblThu2.Text = "Thứ 2";
+            // 
+            // lblThu7
+            // 
+            this.lblThu7.AutoSize = true;
+            this.lblThu7.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblThu7.Location = new System.Drawing.Point(1021, 1);
+            this.lblThu7.Name = "lblThu7";
+            this.lblThu7.Size = new System.Drawing.Size(50, 22);
+            this.lblThu7.TabIndex = 23;
+            this.lblThu7.Text = "Thứ 7";
+            // 
+            // lblThu3
+            // 
+            this.lblThu3.AutoSize = true;
+            this.lblThu3.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblThu3.Location = new System.Drawing.Point(280, 1);
+            this.lblThu3.Name = "lblThu3";
+            this.lblThu3.Size = new System.Drawing.Size(50, 22);
+            this.lblThu3.TabIndex = 24;
+            this.lblThu3.Text = "Thứ 3";
+            // 
+            // lblThu5
+            // 
+            this.lblThu5.AutoSize = true;
+            this.lblThu5.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblThu5.Location = new System.Drawing.Point(634, 1);
+            this.lblThu5.Name = "lblThu5";
+            this.lblThu5.Size = new System.Drawing.Size(50, 22);
+            this.lblThu5.TabIndex = 25;
+            this.lblThu5.Text = "Thứ 5";
+            // 
+            // lblThu4
+            // 
+            this.lblThu4.AutoSize = true;
+            this.lblThu4.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblThu4.Location = new System.Drawing.Point(454, 1);
+            this.lblThu4.Name = "lblThu4";
+            this.lblThu4.Size = new System.Drawing.Size(50, 22);
+            this.lblThu4.TabIndex = 26;
+            this.lblThu4.Text = "Thứ 4";
+            // 
+            // lblChuNhat
+            // 
+            this.lblChuNhat.AutoSize = true;
+            this.lblChuNhat.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblChuNhat.Location = new System.Drawing.Point(1202, 1);
+            this.lblChuNhat.Name = "lblChuNhat";
+            this.lblChuNhat.Size = new System.Drawing.Size(78, 22);
+            this.lblChuNhat.TabIndex = 27;
+            this.lblChuNhat.Text = "Chủ Nhật";
+            // 
+            // frmCapNhatLichLamViec
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1740, 681);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.lblGhiChu);
+            this.Controls.Add(this.txtGhiChu);
+            this.Controls.Add(this.btnSuaLichLamViec);
+            this.Controls.Add(this.btnXoaLichLamViec);
+            this.Controls.Add(this.btnThemLichLamViec);
+            this.Controls.Add(this.btnTroVeCuaCapNhatLichLamViec);
+            this.Controls.Add(this.lblLichLamViec);
+            this.Name = "frmCapNhatLichLamViec";
+            this.Text = "Cập nhât lịch làm việc";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblLichLamViec;
+        private System.Windows.Forms.Button btnTroVeCuaCapNhatLichLamViec;
+        private System.Windows.Forms.Button btnThemLichLamViec;
+        private System.Windows.Forms.Button btnSuaLichLamViec;
+        private System.Windows.Forms.Button btnXoaLichLamViec;
+        private System.Windows.Forms.TextBox txtGhiChu;
+        private System.Windows.Forms.Label lblGhiChu;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label lblCa;
+        private System.Windows.Forms.Label lblCaSang;
+        private System.Windows.Forms.Label lblCaTrua;
+        private System.Windows.Forms.Label lblCaToi;
+        private System.Windows.Forms.Label lblThu2;
+        private System.Windows.Forms.Label lblThu6;
+        private System.Windows.Forms.Label lblThu7;
+        private System.Windows.Forms.Label lblThu3;
+        private System.Windows.Forms.Label lblThu5;
+        private System.Windows.Forms.Label lblThu4;
+        private System.Windows.Forms.Label lblChuNhat;
+    }
+}
